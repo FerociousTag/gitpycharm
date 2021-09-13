8d56b805d9917005add3de7c43486f6ffa8861f0
Zachary Blackwell <88868061+FerociousTag@users.noreply.github.com>
2021-09-12 19:14:38 -0500

    Add files via upload

---
+++ 1.12.py
@@ -0,0 +1,4 @@
+#Zachary Blackwell 1941472
+userNum = int(input())
+userNumSquared = userNum * userNum # Bug here; fix it when instructed
+print(userNumSquared)# Output formatting issue here; fix it when instructed
