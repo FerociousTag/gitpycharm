7f06bba07e639a91fcc02bdfdb1d8c50996224a3
Zachary Blackwell <88868061+FerociousTag@users.noreply.github.com>
2021-12-01 22:09:01 -0600

    Add files via upload

---
+++ Lab 14.11.py
@@ -0,0 +1,15 @@
+#zachary blackwell 1941472
+
+#defines the function
+def selection_sort_descend_trace(user_numbers):
+
+if __name__=="__main__":
+    user_numbers = []
+    #inputs the users number into a string
+    user_numbers_string = input().split(" ")
+    #take the string of numbers and turns it into integers
+    user_numbers = list(map(int, user_numbers_string))
+
+    print(user_numbers_string)
+    print(user_numbers)
+    # selection_sort_descend_trace(user_numbers)
